6bb26ee7a35528cf00d0ed4d41d7a10b1b19235d
Ayaya <kazenosakura100@gmail.com>
2022-10-27 14:34:19 +0700

    Upload

---
+++ customize.sh
@@ -0,0 +1,5 @@
+# Set to true if you need to load system.prop
+PROPFILE=true
+#Print welcome message
+ui_print "- đang flash -- flashing PUBG 90fps Unlock module..."
+ui_print "- module by Bill Nguyen"
